d6322d53681dbc6ae26253c22b45c398893acea8
Paulo Geyer <paulogeyer@gmail.com>
2011-06-15 16:37:41 -0300

    already can get and create repositories

--- main.rb
+++ main.rb
@@ -1,5 +1,21 @@
+require 'fileutils'
 require 'grit'
+require 'json'
 require 'sinatra'
+include Grit
 
-get '/:user/:repo/info' do
+get '/:user/:repo/info' do |user, repo|
+  dir = "repos/#{user}/#{repo}.git"
+  if File.directory?(dir)
+    repo = Repo.new(dir)
+    repo.to_json
+  else
+    not_found
+  end
+end
+
+get '/:user/:repo/create' do |user, repo|
+  dir = "repos/#{user}/#{repo}.git"
+  FileUtils.mkdir_p dir
+  repo = Repo.init_bare(dir)
 end
