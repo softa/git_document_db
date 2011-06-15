a5cb5fb6b150084b402a82413ba667aa89a0c374
Daniel Weinmann <danielweinmann@gmail.com>
2011-06-15 19:24:42 -0300

    Starting to implement the Document class

--- document.rb
+++ document.rb
@@ -1,2 +1,52 @@
+require 'fileutils'
+require 'grit'
+require 'json'
+
+class RepoNotFound < StandardError
+end
+  
 class Document
+
+  include Grit
+  
+  attr_accessor :id, :repo, :attributes
+
+  def initialize id
+    self.id = id
+    self.attributes = {}
+    raise RepoNotFound unless File.directory?(path)
+    self.repo = Repo.new(path)
+  end
+  
+  def destroy
+    FileUtils.rm_rf(path)
+  end
+  
+  def to_json
+    self.attributes.merge({:id => self.id}).to_json
+  end
+
+  def path
+    Document.path id
+  end
+
+  def self.path id
+    "documents/#{id}.git"
+  end
+
+  def self.find id
+    Document.new id
+  rescue
+    return nil
+  end
+  
+  def self.create params
+    id = params[:id]
+    path = self.path(id)
+    FileUtils.mkdir_p path
+    repo = Repo.init_bare(path)
+    # TODO assign attributes, removing the id ;)
+    Document.new id
+  end
+
 end
--- main.rb
+++ main.rb
@@ -1,29 +1,18 @@
-require 'fileutils'
-require 'grit'
-require 'json'
 require 'sinatra'
-include Grit
-
-def doc_path id
-  "documents/#{id}.git"
-end
+require 'document'
 
 get '/documents/:id' do |id|
-  path = doc_path id
-  if File.directory?(path)
-    repo = Repo.new(path)
-    repo.to_json
-  else
-    not_found
-  end
+  document = Document.find id
+  return not_found unless document
+  document.to_json
 end
 
 post '/documents' do
-  path = doc_path id
-  FileUtils.mkdir_p path
-  repo = Repo.init_bare(path)
+  document = Document.create params
+  document.to_json
 end
 
 delete '/documents/:id' do |id|
-  FileUtils.rm_rf(doc_path id)
+  document = Document.find id
+  document.destroy
 end
